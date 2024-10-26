be0ba99e32c762ab10d9478a6dadb0419d88f502
Mustafa KURAL <mustafakural@outlook.com>
2024-10-26 11:57:50 +0300

    sekil

---
+++ u02_cok_bicimlilik/Program.cs
@@ -0,0 +1,23 @@
+﻿using u02_cok_bicimlilik;
+
+Console.WriteLine("Çokbiçimlilik....");
+
+Sekil sekil1 = new Sekil();
+
+Sekil sekil2 = new Kare() {AKenari = 5};
+
+Sekil sekil3 = new Dortgen() {AKenari = 3, BKenari= 4};
+
+
+Console.WriteLine($"Şekil1 in Alanı = {sekil1.AlaniHesapla()}");
+Console.WriteLine($"Şekil2 in Alanı = {sekil2.AlaniHesapla()}");
+Console.WriteLine($"Şekil3 in Alanı = {sekil3.AlaniHesapla()}");
+
+
+
+Kare kare = new Kare(){AKenari = 3};
+
+Console.WriteLine($"Karenin A Kenarı= {kare.AKenari}");//get çalışır
+
+kare.AKenari = 5; //set çalışır
+
---
+++ u02_cok_bicimlilik/Sekil.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+namespace u02_cok_bicimlilik;
+
+public class Sekil
+{
+    public virtual double AlaniHesapla()//virtual - Bu metodu sonradan ezmelerine izin veriyorum
+    {
+        //Alanm hesabını sonraya bırakalım
+        return 0;
+    }
+}
+
+public class Kare : Sekil
+{   
+     public double AKenari { get; set;} //auto property
+
+    public override double AlaniHesapla()
+    {
+        return AKenari * AKenari;
+    }
+}
+
+public class Dortgen: Kare
+{
+     public double BKenari { get; set;} //auto property
+
+    public override double AlaniHesapla()
+    {
+        return AKenari * BKenari;
+    }
+
+}
