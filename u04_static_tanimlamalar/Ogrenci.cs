467454012466ad8083d68190fd5b18be1454af42
Mustafa KURAL <mustafakural@outlook.com>
2024-10-26 14:11:35 +0300

    static

---
+++ u04_static_tanimlamalar/Ogrenci.cs
@@ -0,0 +1,18 @@
+﻿namespace u04_static_tanimlamalar;
+
+public class Ogrenci
+{
+    private int okunanKitapSayisi  = 0; //instance
+    private static int toplamOkunanKitapSayisi = 0;
+
+    public string AdSoyad { get; set; }//auto property
+    public int OkunanKitapSayisi {get {return okunanKitapSayisi;}} 
+    public static int ToplamOkunanKitapSayisi {get {return toplamOkunanKitapSayisi;}} 
+
+    public void KitapOku()
+    {
+        Console.WriteLine($"{AdSoyad} adlı öğrenci kitap okudu.");
+        okunanKitapSayisi++;//ogrenciye ait
+        toplamOkunanKitapSayisi++;//ortak alan
+    }
+}
---
+++ u04_static_tanimlamalar/Program.cs
@@ -0,0 +1,18 @@
+﻿using u04_static_tanimlamalar;
+
+Ogrenci ogr1 = new Ogrenci(){AdSoyad="1.Öğrenci"};
+Ogrenci ogr2 = new Ogrenci(){AdSoyad="2.Öğrenci"};
+Ogrenci ogr3 = new Ogrenci(){AdSoyad="3.Öğrenci"};
+Ogrenci ogr4 = new Ogrenci(){AdSoyad="4.Öğrenci"};
+
+ogr1.KitapOku();
+ogr2.KitapOku();
+ogr1.KitapOku();
+ogr3.KitapOku();
+
+Console.WriteLine($"{ogr1.AdSoyad} okunan kitap sayısı = {ogr1.OkunanKitapSayisi}");
+Console.WriteLine($"{ogr2.AdSoyad} okunan kitap sayısı = {ogr2.OkunanKitapSayisi}");
+Console.WriteLine($"{ogr3.AdSoyad} okunan kitap sayısı = {ogr3.OkunanKitapSayisi}");
+Console.WriteLine($"{ogr4.AdSoyad} okunan kitap sayısı = {ogr4.OkunanKitapSayisi}");
+
+Console.WriteLine($"Toplam okunan kitap sayısı = {Ogrenci.ToplamOkunanKitapSayisi}");
