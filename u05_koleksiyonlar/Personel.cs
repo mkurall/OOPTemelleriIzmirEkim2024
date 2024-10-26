a4a446f2d79674b8690fc3fde07b4f6977c5d091
Mustafa KURAL <mustafakural@outlook.com>
2024-10-26 14:26:38 +0300

    array list

---
+++ u05_koleksiyonlar/Personel.cs
@@ -0,0 +1,12 @@
+﻿namespace u05_koleksiyonlar;
+
+public class Personel : object
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+
+    public override string ToString()
+    {
+        return Ad + " " + Soyad;
+    }
+}
---
+++ u05_koleksiyonlar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using u05_koleksiyonlar;
+
+//array list içindeki herşey bir objectir.
+ArrayList arrayList = new ArrayList();
+
+arrayList.Add(3);//ekelemek
+arrayList.Add("Btk");
+arrayList.Add(true);
+arrayList.Add(new Personel() { Ad = "Kerem", Soyad="Koyuncu"});
+
+Personel p = (Personel)arrayList[3];
+int sayi = (int)arrayList[0];
+
+arrayList.RemoveAt(2);//silmek
+
+foreach(object elem in arrayList)
+{
+    Console.WriteLine(elem);
+}
+
