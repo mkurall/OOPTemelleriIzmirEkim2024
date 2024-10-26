cfbf140d7d39632eeaf7f52c872ee2954e530add
Mustafa KURAL <mustafakural@outlook.com>
2024-10-26 10:31:10 +0300

    first commit

---
+++ u01_oop_ilk_uygulama/Personel.cs
@@ -0,0 +1,26 @@
+﻿namespace u01_oop_ilk_uygulama;//personel sınıfı bu uzayda olsun
+
+public class Personel
+{
+    //Fields(alanlar)
+    private int id;//dışaıdan erişilemez
+    private string ad;
+    private string soyad;
+
+    //Properties(Özellikler)
+    public int Id { get { return id; } }
+    public string Ad { get { return ad; } set { ad = value; } }
+    public string Soyad { get { return soyad; } set { soyad = value; } }
+
+    //Constructor (Yapıcı metotlar)
+    public Personel(int personelId)
+    {
+        id = personelId;
+    }
+
+    //Methods (metotlar)
+    public void Yazdir()
+    {
+        Console.WriteLine($"{id} - {ad} {soyad}");
+    }
+}
---
+++ u01_oop_ilk_uygulama/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using u01_oop_ilk_uygulama;//personel sınıfının uzayını kullan
+
+Personel p1 = new Personel(1);//bellekten p1 için yer ayır
+p1.Ad = "Hasan";
+p1.Soyad = "DEMİR";
+
+Personel p2 = new Personel(2) { Ad = "Ayşe", Soyad = "CAN"};
+
+//p1.Id = 5; Id sadece okunabilir olduğu için değiştirilemez
+p1.Ad = "Mehmet";
+
+Console.WriteLine("Personel1'in Bilgileri");
+p1.Yazdir();
+
+Console.WriteLine("\n\nPersonel2'in Bilgileri");
+p2.Yazdir();
