2020d688ba0f75405a20322ef5209341908e6a85
Mustafa KURAL <mustafakural@outlook.com>
2024-10-26 15:22:23 +0300

    yığın ve kuysyk

---
+++ u08_stack_ve_queue/Program.cs
@@ -0,0 +1,24 @@
+﻿Queue<int> kuyruk = new Queue<int>();
+
+kuyruk.Enqueue(3);
+kuyruk.Enqueue(8);
+kuyruk.Enqueue(2);
+
+Console.WriteLine("Kuyruk:");
+while(kuyruk.Count > 0)
+{
+    Console.WriteLine(kuyruk.Dequeue());//kuyruğun ilk elemanını getirir ve kuyruktan siler
+}
+
+Stack<int> yigin = new Stack<int>();
+
+yigin.Push(3);
+yigin.Push(8);
+yigin.Push(2);
+
+Console.WriteLine("Yığın:");
+
+while(yigin.Count > 0)
+{
+    Console.WriteLine(yigin.Pop());//yığından elemanı alır ve siler
+}
