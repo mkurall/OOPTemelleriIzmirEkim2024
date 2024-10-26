3b7fdb5543d7b96e50d6d02a5af21410b3c479de
Mustafa KURAL <mustafakural@outlook.com>
2024-10-26 15:47:37 +0300

    directory

---
+++ u10_klasor_islemleri/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] klasorler = Directory.GetDirectories("C:\\");
+
+foreach(string klasor in klasorler)
+{
+    Console.WriteLine(klasor);
+}
+
+if(!Directory.Exists("C:\\BTKAkdemi"))
+{
+    Directory.CreateDirectory("C:\\BTKAkademi");
+}
+
+Directory.Delete("C:\\BTKAkademi");
+
+string masaustuYolu =  Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+Console.WriteLine("Masaüstü: " + masaustuYolu);
+
+Directory.CreateDirectory(Path.Combine(masaustuYolu,"BTK"));
