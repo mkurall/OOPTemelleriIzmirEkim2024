4c47e9c77f28e26e43b061896e49deba5eb2f247
Mustafa KURAL <mustafakural@outlook.com>
2024-10-26 15:33:54 +0300

    path

---
+++ u09_dosyalama1/Program.cs
@@ -0,0 +1,10 @@
+﻿string yol1 = "C:\\Users\\";
+string yol2 = "Belgeler\\dosya.txt";
+
+string yol3 = Path.Combine(yol1, yol2);
+
+Console.WriteLine("Path:" + yol3);
+
+Console.WriteLine("DirectoryName:" + Path.GetDirectoryName(yol3));
+Console.WriteLine("FileName:" + Path.GetFileName(yol3));
+Console.WriteLine("Extension:" + Path.GetExtension(yol3));
