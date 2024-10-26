2c11acebdff294f4a84e1c414d12bd889e8b38a6
Mustafa KURAL <mustafakural@outlook.com>
2024-10-26 15:17:10 +0300

    xxxx

---
+++ u07_sozlukler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+Dictionary<int, string> liste = new Dictionary<int, string>();
+
+liste.Add(1, "Adana");
+liste.Add(6,"Ankara");
+liste.Add(34, "İstanbul");
+liste.Add(35, "İzmir");
+
+//liste.Add(99, "Adana");
+
+liste[10] = "Balıkesir";//10 index değil
+liste[9] = "Aydın";
+
+Console.WriteLine(liste[35]);//35 anahtarına karşılık gelen değer
+
+foreach(var elem in liste)
+{
+    Console.WriteLine($"Anahtar: {elem.Key} Değer: {elem.Value}");
+}
+
+
+
+
