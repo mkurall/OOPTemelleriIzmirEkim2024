18f8e15bd63989636fbae65b6f85a3e570cce4eb
Mustafa KURAL <mustafakural@outlook.com>
2024-10-26 16:22:49 +0300

    hata yönetimi

---
+++ u12_hata_yonetimi/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Bir sayı girin:");
+int sayi = 0;
+bool kontrol = false;
+
+do
+{
+    try
+    {
+        sayi = Convert.ToInt32(Console.ReadLine());
+       kontrol = true;
+    }
+
+    catch(FormatException hata)
+    {
+        Console.WriteLine("Lütfen sayı girin!"); 
+    }
+    catch(OverflowException hata)
+    {
+        Console.WriteLine("Lütfen aralıklara uygun sayı girin!"); 
+    }
+    catch(Exception hata)
+    {
+        Console.WriteLine(hata.Message); 
+    }
+
+} while(kontrol == false);
+
+Console.WriteLine($"Sayının Karesi = {sayi * sayi}");
